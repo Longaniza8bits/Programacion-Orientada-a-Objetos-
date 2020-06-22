5b4d06ef2634d63ae43b419c9984cc3df08df050
Longaniza8bits <62490520+Longaniza8bits@users.noreply.github.com>
2020-06-22 17:24:02 -0500

    Ejercicio 5 y ejercicio 6
    Unete

---
+++ Matriz multiplicada/src/matriz/multiplicada/MatrizMultiplicada.java
@@ -0,0 +1,18 @@
+package matriz.multiplicada;
+
+
+public class MatrizMultiplicada {
+
+  
+    public static void main(String[] args) {
+Matriz e1=new Matriz();
+e1.matriz();
+e1.imprimir();
+ 
+    }   
+}
+        
+    
+    
+
+    
